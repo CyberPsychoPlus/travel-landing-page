d2a9f5e362c116845f5821de51b0b1ecc1d51fdd
Nick <deaneygaming@gmail.com>
2023-11-13 17:26:19 +0200

    small changes

--- js/main.js
+++ js/main.js
@@ -1,31 +1,31 @@
 // Header animation
 (function () {
-  const header = document.querySelector(".header");
+  const header = document.querySelector('.header');
   window.onscroll = () => {
     if (window.scrollY > 50) {
-      header.classList.add("header_active");
+      header.classList.add('header_active');
     } else {
-      header.classList.remove("header_active");
+      header.classList.remove('header_active');
     }
   };
 })();
 
 // Burger
 (function () {
-  const burgerItem = document.querySelector(".burger");
-  const menu = document.querySelector(".header__nav");
-  const menuCloseItem = document.querySelector(".header__nav-close");
-  const menuLinks = document.querySelectorAll(".header__link");
-  burgerItem.addEventListener("click", () => {
-    menu.classList.add("header__nav_active");
+  const burgerItem = document.querySelector('.burger');
+  const menu = document.querySelector('.header__nav');
+  const menuCloseItem = document.querySelector('.header__nav-close');
+  const menuLinks = document.querySelectorAll('.header__link');
+  burgerItem.addEventListener('click', () => {
+    menu.classList.add('header__nav_active');
   });
-  menuCloseItem.addEventListener("click", () => {
-    menu.classList.remove("header__nav_active");
+  menuCloseItem.addEventListener('click', () => {
+    menu.classList.remove('header__nav_active');
   });
   if (window.innerWidth <= 767) {
     for (let i = 0; i < menuLinks.length; i += 1) {
-      menuLinks[i].addEventListener("click", () => {
-        menu.classList.remove("header__nav_active");
+      menuLinks[i].addEventListener('click', () => {
+        menu.classList.remove('header__nav_active');
       });
     }
   }
@@ -34,7 +34,7 @@
 // Scroll to anchors
 (function () {
   const smoothScroll = function (targetEl, duration) {
-    const headerElHeight = document.querySelector(".header").clientHeight;
+    const headerElHeight = document.querySelector('.header').clientHeight;
     let target = document.querySelector(targetEl);
     let targetPosition = target.getBoundingClientRect().top - headerElHeight;
     let startPosition = window.pageYOffset;
@@ -58,10 +58,10 @@
   };
 
   const scrollTo = function () {
-    const links = document.querySelectorAll(".js-scroll");
+    const links = document.querySelectorAll('.js-scroll');
     links.forEach((each) => {
-      each.addEventListener("click", function () {
-        const currentTarget = this.getAttribute("href");
+      each.addEventListener('click', function () {
+        const currentTarget = this.getAttribute('href');
         smoothScroll(currentTarget, 1000);
       });
     });
